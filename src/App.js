81cf64ed8ebf90cc3eac9603f3694e1a6083ff64
arthurstuart <arthur.stuart10@gmail.com>
2021-03-22 01:39:39 -0400

    will continue to work in the am

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import React, [ useState] from 'react'
+import books from '.books'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [bookquery, setquery] = useState{books}
 }
 
-export default App;
+const manageData = (title, minimized) = {
+    const additionalInfo = bookquery.map(book)
+        if (book.title === title) {
+            return{book, minimized}
+        }
+}
+const Books = (props) => {
+    const { title, author, minimized, shortDescription, coverImageUrl, publisher, publicationDate, detailedDescription }
+    const [book, idx] = useState
+    return (
+        <div className='list_of_books'>
+            <h1>List of Books to Read</h1>
+                {books.map((book)) =>
+                    <book
+                        title = {book.title}
+                        author = {book.author}
+                        shortDescription = {book.shortDescription}
+                        coverImageUrl = {book.coverImageUrl}
+                        url = {book.url}
+                        key={idx}/>
+                        <button onClick = {() => 
+                          className='more-info'  
+
+                        }
+
+                    >
+                }
+
+
+</div>
+
+    )
+}
+
+{
